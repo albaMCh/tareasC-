efb5179e7938e77a881087766295d42eced1dc55
albamolera <albamolerachacon@gmail.com>
2022-06-27 17:11:59 +0200

    Tarea

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Introduce tu nombre: ");
+
+string nombre= Console.ReadLine();
+
+
+Console.WriteLine("Hello, " + nombre);
+
+string datetime = DateTime.Now.ToString("hh:mm:ss tt");
+            Console.WriteLine(datetime);
